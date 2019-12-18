b53c26c38f25f1f88fa05f5d5ffe2a9b330d376c
censhengde <37496351+censhengde@users.noreply.github.com>
2019-12-18 19:17:41 +0800

    第一次提交，目前尚且完成生成二维码一个接口，其他有待完善

---
+++ app/src/main/java/com/ringle/qrcoder/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ringle.qrcoder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ringle.xinpay.common.qrcode.QRCodeHelper
+import kotlinx.android.synthetic.main.fragment_receive_qrcode.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_receive_qrcode)
+        initQRCode()
+    }
+
+   /**
+    *初始化二维码
+    */
+    private fun initQRCode(){
+        QRCodeHelper.encodeQRCode("收款地址",logoResId = R.drawable.resources_img_investment_investment_train_icon,callback={
+            //it就是回调的bitmap
+            iv_qr_code.setImageBitmap(it)
+        })
+    }
+}
---
+++ app/src/main/java/com/ringle/qrcoder/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.ringle.qrcoder
+
+import android.app.Application
+import com.ringle.qrcoder.qrcode.ActualQRCoder
+import com.ringle.xinpay.common.qrcode.QRCodeHelper
+
+/**
+ * create by 岑胜德
+ * on 2019/12/18
+ * 说明:
+ *
+ */
+class MyApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //初始化框架
+        QRCodeHelper.init(ActualQRCoder(this))
+    }
+}
